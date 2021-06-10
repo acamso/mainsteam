a059a8d6715b96add6e7ce783725dc2b004bd51c
acamso <acamso@gmail.com>
2021-06-10 19:33:13 -0400

    extend module docstring

--- flask/basic_app.py
+++ flask/basic_app.py
@@ -3,7 +3,14 @@
 # 06-10-2021 18:41:52 EDT
 # (c) 2021 acamso
 
-"""Basic Flask application."""
+"""Basic Flask application.
+
+This is a basic Flask application with a single route returning basic HTML. The app can be ran from
+terminal using:
+
+$ export FLASK_APP=basic_app
+$ flask run
+"""
 
 from flask import Flask
 
